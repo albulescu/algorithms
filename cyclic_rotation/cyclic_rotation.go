6bcd02f30a9da88fb335f97e46643ef908b5ef60
Cosmin Albulescu <cosmin@albulescu.ro>
2016-02-18 10:46:45 +0200

    Cyclic rotation

--- cyclic_rotation/cyclic_rotation.go
+++ cyclic_rotation/cyclic_rotation.go
@@ -1,9 +1,19 @@
 package main
 
-func CyclicRotation(A []int, K int) []int {
-	r := make([]int, 0)
-	for i, e := range A {
-		r[i] = e
+//import "fmt"
+
+func Solution(A []int, K int) []int {
+
+	size := len(A)
+	r := make([]int, size)
+
+	if size < 1 {
+		return A
 	}
+
+	for i := 0; i < size; i++ {
+		r[(i+K)%size] = A[i]
+	}
+
 	return r
 }
--- cyclic_rotation/cyclic_rotation_test.go
+++ cyclic_rotation/cyclic_rotation_test.go
@@ -2,23 +2,31 @@ package main
 
 import "testing"
 
-func AssertCyclicRotation(a []int, shift int, out []int, t *testing.T) bool {
+func ValidateSolution(a []int, shift int, out []int, t *testing.T) (bool, []int) {
 
-	result := CyclicRotation(a, shift)
+	result := Solution(a, shift)
 
 	if len(result) != len(out) {
-		return false
+		return false, result
 	}
 
-	for i := range result {
+	for i, _ := range result {
 		if result[i] != out[i] {
-			return false
+			return false, result
 		}
 	}
 
-	return true
+	return true, result
 }
 
-func TestCyclicRotation(t *testing.T) {
-	AssertCyclicRotation([]int{1, 2, 3, 4, 5}, 2, []int{1, 2, 3, 4, 5}, t)
+func Assert(a []int, shift int, out []int, t *testing.T) {
+	ok, result := ValidateSolution(a, shift, out, t)
+	if !ok {
+		t.Error("Fail to rotate", shift, " times. Expected: ", out, "Result:", result)
+	}
+}
+
+func TestSolution(t *testing.T) {
+	Assert([]int{1, 2, 3, 4}, 1, []int{4, 1, 2, 3}, t)
+	Assert([]int{1, 2, 3, 4}, 2, []int{3, 4, 1, 2}, t)
 }
